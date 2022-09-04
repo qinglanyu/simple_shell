9f1ec3efd26717e2fcd478f76d02ded4c201ba22
qinglanyu <qinglanyu_jun@foxmail.com>
2022-09-04 13:36:42 +0800

    remove inline flag to compile under vs2013

--- printf.c
+++ printf.c
@@ -130,7 +130,7 @@ typedef struct {
 
 
 // internal buffer output
-static inline void _out_buffer(char character, void* buffer, size_t idx, size_t maxlen)
+static void _out_buffer(char character, void* buffer, size_t idx, size_t maxlen)
 {
     if (idx < maxlen) {
         ((char*)buffer)[idx] = character;
@@ -139,14 +139,14 @@ static inline void _out_buffer(char character, void* buffer, size_t idx, size_t 
 
 
 // internal null output
-static inline void _out_null(char character, void* buffer, size_t idx, size_t maxlen)
+static void _out_null(char character, void* buffer, size_t idx, size_t maxlen)
 {
     (void)character; (void)buffer; (void)idx; (void)maxlen;
 }
 
 
 // internal _putchar wrapper
-static inline void _out_char(char character, void* buffer, size_t idx, size_t maxlen)
+static void _out_char(char character, void* buffer, size_t idx, size_t maxlen)
 {
     (void)buffer; (void)idx; (void)maxlen;
     if (character) {
@@ -156,7 +156,7 @@ static inline void _out_char(char character, void* buffer, size_t idx, size_t ma
 
 
 // internal output function wrapper
-static inline void _out_fct(char character, void* buffer, size_t idx, size_t maxlen)
+static void _out_fct(char character, void* buffer, size_t idx, size_t maxlen)
 {
     (void)idx; (void)maxlen;
     if (character) {
@@ -168,7 +168,7 @@ static inline void _out_fct(char character, void* buffer, size_t idx, size_t max
 
 // internal secure strlen
 // \return The length of the string (excluding the terminating 0) limited by 'maxsize'
-static inline unsigned int _strnlen_s(const char* str, size_t maxsize)
+static unsigned int _strnlen_s(const char* str, size_t maxsize)
 {
     const char* s;
     for (s = str; *s && maxsize--; ++s);
@@ -178,7 +178,7 @@ static inline unsigned int _strnlen_s(const char* str, size_t maxsize)
 
 // internal test if char is a digit (0-9)
 // \return true if char is a digit
-static inline bool _is_digit(char ch)
+static bool _is_digit(char ch)
 {
     return (ch >= '0') && (ch <= '9');
 }
